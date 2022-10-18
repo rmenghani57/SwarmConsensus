360240643d926909988234854ae2c9166a968bd3
Rohit Menghani <rmenghani@sollensys.com>
2022-10-18 01:56:57 -0400

    some pub subs, skeleton of cases

--- Translator/manual_translation/src/iq_gnc/src/Drone.cpp
+++ Translator/manual_translation/src/iq_gnc/src/Drone.cpp
@@ -1,14 +1,75 @@
 #include "base_classes/Drone_base_class.cpp"
+#include "ros/ros.h"
 #include <thread>
 #include <chrono>
 using namespace std;
 
+
+void statusCallback(int msg){
+
+    ROS_INFO("update_stautus = [%d]", msg);
+
+}
+
 int main(int argc, char** argv)
 {
 
+    // initialize ROS
+    ros::init(argc, argv, "MissionControl_node");
+    // private node handle
+    ros::NodeHandle nh("~");
+
+    // explanation on this?
+    // This function is called at the beginning of a program and will start of the communication links to the FCU. The function requires the program's ros nodehandle as an input 
+    // This function takes our ros node handle as an input and initializes subcribers that will collect the necessary information from our autopilot. 
+    // @returns n/a
+    init_publisher_subscriber(nh);
+
+    //whenever new message in topic update_status, statusCallback func is called
+    ros::Subscriber update_status_sub = nh.subscribe("update_status", 1, statusCallback);
+
+    // do i instantiate 8 drone objects? but there are already 8 drone nodes in launch file, so this file will run 8 times in parallel?
+
+    // rate of 1 Hz
+    //frequency that you would like to loop at. It will keep track of how long it has been since the last call to Rate::sleep(), and sleep for the correct amount of time.
+    ros::Rate rate(1.0);
 
 
-    ros::Subscriber update_status_sub = nh.subscribe("update_status", 1)
+    // define TA states as enum
+    enum STATES 
+    {
+        Idle, InSwarm, Leader, UpdatingLocation. WaitingSwarm 
+    } STATE;
+
+    STATE = Idle;
+
+    while(ros::ok()){
+
+        ros::spinOnce();
+
+        switch(STATE){
+
+            case Idle:
+
+                break;     
+
+            case InSwarm:
+                break;
+
+            case Leader:
+                break;
+
+            case UpdatingLocation:
+                break;  
+
+            case WaitingSwarm:
+                break;
+
+        }
+
+
+    }
+    
 
 
 
--- Translator/manual_translation/src/iq_gnc/src/MissionControl.cpp
+++ Translator/manual_translation/src/iq_gnc/src/MissionControl.cpp
@@ -1,4 +1,5 @@
 #include "base_classes/MissionControl_base_class.cpp"
+#include "ros/ros.h"
 #include <thread>
 #include <chrono>
 using namespace std;
@@ -15,8 +16,10 @@ int main(int argc, char** argv)
 
     init_publisher_subscriber(nh);
 
+    // all publishers - each represents a channel in uppaal
     // this node publishes to the update_status topic which is an Uppaal channel (triggered in MissionControl Template)
     ros::Publisher update_status_pub = nh.advertise<int>("update_status", 1);
+    ros::Publisher member_election_pub = nh.advertise<int>("update_status", 1);
 
     // rate of 1 Hz
     //frequency that you would like to loop at. It will keep track of how long it has been since the last call to Rate::sleep(), and sleep for the correct amount of time.
@@ -38,18 +41,44 @@ int main(int argc, char** argv)
         switch(STATE){
 
             case Start:
-                nh.setParam("/goal_location", nh.getParam(goal);
+                nh.setParam("/goal_location", nh.getParam(goal));
                 STATE = CheckMembers;
                 break;
                 
             case CheckMembers:
-                if(nh.getParam(updating_mission) == true){
+                if(nh.getParam("/updating_mission") == true){
                     MissionController->possible_member();
-
+                    update_status_pub.publish(1);
                 }
+                STATE = ElectMembers;
+                break;
 
+            case ElectMembers:
+                if(nh.getParam("/updating_mission") == true){
+                    nh.setParameter("/updating_mission", false)
+                    member_election.publish(1);
+                }
+                if(nh.getParam("/updating_mission") == false){
+                    MissionController.elect_members();
+                    STATE = UpdateMembers;
+                }
+                break;
 
+            case UpdateMembers:
+                
+                break;     
 
+            case LeaderElection:
+                
+                break;
+
+            case MissionStarted:
+            
+                break;
+
+            case MissionAccomplished:
+            
+                break;
 
 
 
