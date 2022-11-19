446e92b0ad4755208f3c676d8b38144e2b1754f5
Rohit Menghani <rmenghani@sollensys.com>
2022-11-19 17:57:25 -0500

    FCU connection lines set mode to guided

--- Translator/manual_translation/src/iq_gnc/src/Drone.cpp
+++ Translator/manual_translation/src/iq_gnc/src/Drone.cpp
@@ -61,6 +61,12 @@ int main(int argc, char** argv)
     // private node handle
     ros::NodeHandle nh("~");
 
+    // wait for FCU connection
+    wait4connect();
+    
+    // changing mode to GUIDED 
+    set_mode("GUIDED");
+
     // explanation on this?
     // This function is called at the beginning of a program and will start of the communication links to the FCU. The function requires the program's ros nodehandle as an input 
     // This function takes our ros node handle as an input and initializes subcribers that will collect the necessary information from our autopilot. 
