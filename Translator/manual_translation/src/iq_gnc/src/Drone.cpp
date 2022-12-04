ce47549d45b10dcde2782ebb89086ce55c81876a
Rohit Menghani <rmenghani@sollensys.com>
2022-12-03 20:31:46 -0500

    typo fix

--- Translator/manual_translation/src/iq_gnc/src/Drone.cpp
+++ Translator/manual_translation/src/iq_gnc/src/Drone.cpp
@@ -200,12 +200,14 @@ int main(int argc, char** argv)
                         member_election_sub.shutdown();
                         member_election_var = 0;
                         nh.getParam("/vote_counter", vote_counter);
-                        while(vote_counter =! 0){
+                        while(vote_counter != 0){
                         // wait till member election sets votes back to 0
                             nh.getParam("/vote_counter", vote_counter);
                         }
                     }
 
+
+                    ROS_INFO("drones out of member election, moving to leader election");
                     nh.getParam("/vote_counter", vote_counter);
                     leader_election_sub = nh.subscribe("/leader_election", 1, leaderElectionCallback);
                     if(leader_election_var == 1 && vote_counter < 3){       // addint vote counter constraint    
